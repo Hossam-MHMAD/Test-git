7ec92696f27fc501ebde55f2c849a2ba4d3828d6
Hossam-MHMAD <hossamtulimat2009@gmail.com>
2025-08-28 22:46:36 +0300

    Adding Some Functions

--- test_main.py
+++ test_main.py
@@ -1,3 +1,8 @@
+import os
+
+
+def load_imgs(img_path):
+  pass
 # from main import greater_than_10, divide
 # import pytest
 
@@ -6,5 +11,7 @@
 # 	assert greater_than_10(44), 'Number Is Low Than 10'
 # 	assert greater_than_10(40), 'Number Is Low Than 10'
 
+def load_imgs(imgs_path):
+  pass
 # def test_divide():
 # 	with pytest.raises(ValueError):
