bda802d742213a4a0291491c58f7fb48c77f9d77
Hossam-MHMAD <hossamtulimat2009@gmail.com>
2025-08-28 22:45:30 +0300

    Adding Something

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pygame
+import sys
+
 def greater_than_10(num):
 	return num > 10
 
@@ -11,6 +14,7 @@ class Calculater:
 	def __init__(self, num1, num2):
 		self.num1 = num1
 		self.num2 = num2
+		self.add = self.num1 + self.num2
 
 	def add(self): 
 		return self.num1 + self.num2
@@ -25,4 +29,9 @@ class Calculater:
 	
 	def product(self):
 		return self.num1 * self.num2
+
+def pygame_game():
+	pass
+		
 	
+
