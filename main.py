c7b8bfd17e1d15335de3b0304a0c48e5752dbb7b
Hossam-MHMAD <hossamtulimat2009@gmail.com>
2025-08-28 22:40:05 +0300

    Modify Files

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ def divide(a, b):
 	if b == 0:
 		raise ValueError
 	return a / b
+
+
+class Calculater:
+	def __init__(self, num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+
+	def add(self): 
+		return self.num1 + self.num2
+	
+	def difference(self):
+		return self.num1 - self.num2
+	
+	def divide(self):
+		if self.num2 != 0:
+			return self.num1 / self.num2
+		raise ZeroDivisionError('Cannot divide by zero!!')
+	
+	def product(self):
+		return self.num1 * self.num2
+	
--- test_main.py
+++ test_main.py
@@ -1,11 +1,10 @@
-from main import greater_than_10, divide
-import pytest
+# from main import greater_than_10, divide
+# import pytest
 
-def test_greater_than_10():
-	assert greater_than_10(55), 'Number Is Low Than 10'
-	assert greater_than_10(44), 'Number Is Low Than 10'
-	assert greater_than_10(40), 'Number Is Low Than 10'
+# def test_greater_than_10():
+# 	assert greater_than_10(55), 'Number Is Low Than 10'
+# 	assert greater_than_10(44), 'Number Is Low Than 10'
+# 	assert greater_than_10(40), 'Number Is Low Than 10'
 
-def test_divide():
-	with pytest.raises(ValueError):
-		divide(10, 0)
+# def test_divide():
+# 	with pytest.raises(ValueError):
